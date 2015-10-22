c456e94be3415c0a50bcbc046acde9b43b2120fd
中软mini003 <zhongruanmini003@zhongruanmini003deMac-mini.local>
2015-10-22 13:34:50 +0800

    Initial Commit

---
+++ NotifactionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NotifactionDemo
+//
+//  Created by 中软mini003 on 15/10/22.
+//  Copyright (c) 2015年 中软mini003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NotifactionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NotifactionDemo
+//
+//  Created by 中软mini003 on 15/10/22.
+//  Copyright (c) 2015年 中软mini003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
